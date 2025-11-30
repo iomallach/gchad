fe5befc29f387bbd6cfc7141c930f4c4c51db2e8
Alexander Butenko <a.butenko.o@gmail.com>
2025-11-30 22:25:20 +0100

    Add client registry unit test

---
+++ tests/internal/application/client_registry_test.go
@@ -0,0 +1,42 @@
+package application_test
+
+import (
+	"testing"
+
+	"github.com/iomallach/gchad/internal/application"
+	"github.com/iomallach/gchad/internal/domain"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClientRegistry_AddClient_GetClient(t *testing.T) {
+	registry := application.NewClientRegistry()
+	client := domain.NewClient("1", "Jane Doe")
+
+	registry.AddClient(client)
+
+	assert.Equal(t, client, registry.GetClient(client.Id()))
+}
+
+func TestClientRegistry_RemoveClient(t *testing.T) {
+	registry := application.NewClientRegistry()
+	client := domain.NewClient("1", "Jane Doe")
+
+	registry.AddClient(client)
+
+	assert.Equal(t, client, registry.GetClient(client.Id()))
+
+	registry.RemoveClient(client.Id())
+
+	assert.Equal(t, []*domain.Client{}, registry.GetAllClients())
+}
+
+func TestClientRegistry_GetAllClients(t *testing.T) {
+	registry := application.NewClientRegistry()
+	client1 := domain.NewClient("1", "Jane Doe")
+	client2 := domain.NewClient("2", "John Doe")
+
+	registry.AddClient(client1)
+	registry.AddClient(client2)
+
+	assert.Equal(t, []*domain.Client{client1, client2}, registry.GetAllClients())
+}
