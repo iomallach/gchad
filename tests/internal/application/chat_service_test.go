973138bab55c0107d755cf5561645db90ea0db3a
Alexander Butenko <a.butenko.o@gmail.com>
2025-12-08 00:03:43 +0100

    Refactor chat service tests

--- tests/internal/application/chat_service_test.go
+++ tests/internal/application/chat_service_test.go
@@ -35,103 +35,168 @@ func (s *ErrorNotifier) BroadCastToRoom(room *application.ChatRoom, msg domain.M
 	return nil
 }
 
-func TestChatService_EnterRoom_Success(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	clientRegistry := application.NewClientRegistry()
-	room := application.NewChatRoom("1", "general", clientRegistry)
-	frozenTime := time.Date(2025, 12, 7, 0, 0, 0, 0, time.UTC)
-	expectedBroadcasts := []struct {
-		joined *domain.UserJoinedSystemMessage
-		room   *application.ChatRoom
+func TestChatService_EnterRoom(t *testing.T) {
+	tests := []struct {
+		name               string
+		clients            []struct{ id, name string }
+		expectedBroadcasts func(*application.ChatRoom, time.Time) []Broadcast
 	}{
 		{
-			joined: &domain.UserJoinedSystemMessage{
-				Timestamp: frozenTime,
-				Name:      "Jane Doe",
+			name: "success with two users joining",
+			clients: []struct{ id, name string }{
+				{"1", "Jane Doe"},
+				{"2", "John Doe"},
 			},
-			room: room,
-		},
-		{
-			joined: &domain.UserJoinedSystemMessage{
-				Timestamp: frozenTime,
-				Name:      "John Doe",
+			expectedBroadcasts: func(room *application.ChatRoom, frozenTime time.Time) []Broadcast {
+				return []Broadcast{
+					{
+						room: room,
+						msg: &domain.UserJoinedSystemMessage{
+							Timestamp: frozenTime,
+							Name:      "Jane Doe",
+						},
+					},
+					{
+						room: room,
+						msg: &domain.UserJoinedSystemMessage{
+							Timestamp: frozenTime,
+							Name:      "John Doe",
+						},
+					},
+				}
 			},
-			room: room,
 		},
 	}
 
-	spyNotifier := SpyNotifier{broadcasts: make([]Broadcast, 0)}
-	chatService := application.NewChatService(room, &spyNotifier, func() time.Time { return frozenTime }, 3, 3)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
 
-	chatService.Start(ctx)
+			clientRegistry := application.NewClientRegistry()
+			room := application.NewChatRoom("1", "general", clientRegistry)
+			frozenTime := time.Date(2025, 12, 7, 0, 0, 0, 0, time.UTC)
 
-	chatService.EnterRoom("1", "Jane Doe")
-	chatService.EnterRoom("2", "John Doe")
+			spyNotifier := SpyNotifier{broadcasts: make([]Broadcast, 0)}
+			chatService := application.NewChatService(room, &spyNotifier, func() time.Time { return frozenTime }, 3, 3)
 
-	time.Sleep(50 * time.Millisecond)
+			chatService.Start(ctx)
 
-	for idx, broadcast := range spyNotifier.broadcasts {
-		userJoined, ok := broadcast.msg.(*domain.UserJoinedSystemMessage)
-		if !ok {
-			t.Errorf("expected a user joined message, got %T", broadcast)
-		}
-		expected := expectedBroadcasts[idx]
-		assert.Equal(t, expected.joined, userJoined)
-		assert.Equal(t, expected.room, broadcast.room)
+			for _, client := range tt.clients {
+				chatService.EnterRoom(client.id, client.name)
+			}
+
+			time.Sleep(50 * time.Millisecond)
+
+			expectedBroadcasts := tt.expectedBroadcasts(room, frozenTime)
+			assert.Equal(t, len(expectedBroadcasts), len(spyNotifier.broadcasts))
+
+			for idx, broadcast := range spyNotifier.broadcasts {
+				userJoined, ok := broadcast.msg.(*domain.UserJoinedSystemMessage)
+				if !ok {
+					t.Errorf("expected a user joined message, got %T", broadcast)
+				}
+				assert.Equal(t, expectedBroadcasts[idx].msg, userJoined)
+				assert.Equal(t, expectedBroadcasts[idx].room, broadcast.room)
+			}
+		})
 	}
 }
 
-func TestChatService_LeaveRoom_Success(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	clientRegistry := application.NewClientRegistry()
-	room := application.NewChatRoom("1", "general", clientRegistry)
-	frozenTime := time.Date(2025, 12, 7, 0, 0, 0, 0, time.UTC)
-
-	expectedBroadcasts := []struct {
-		left *domain.UserLeftSystemMessage
-		room *application.ChatRoom
+func TestChatService_LeaveRoom(t *testing.T) {
+	tests := []struct {
+		name                 string
+		clientsIn            []struct{ id, name string }
+		clientsOut           []string
+		expectedBroadcasts   func(*application.ChatRoom, time.Time) []Broadcast
+		expectedClientsCount int
 	}{
 		{
-			left: &domain.UserLeftSystemMessage{
-				Timestamp: frozenTime,
-				Name:      "Jane Doe",
+			name: "success with two users entering and leaving",
+			clientsIn: []struct{ id, name string }{
+				{"1", "Jane Doe"},
+				{"2", "John Doe"},
 			},
-			room: room,
+			clientsOut: []string{"1", "2"},
+			expectedBroadcasts: func(room *application.ChatRoom, frozenTime time.Time) []Broadcast {
+				return []Broadcast{
+					{
+						room: room,
+						msg: &domain.UserLeftSystemMessage{
+							Timestamp: frozenTime,
+							Name:      "Jane Doe",
+						},
+					},
+					{
+						room: room,
+						msg: &domain.UserLeftSystemMessage{
+							Timestamp: frozenTime,
+							Name:      "John Doe",
+						},
+					},
+				}
+			},
+			expectedClientsCount: 0,
 		},
 		{
-			left: &domain.UserLeftSystemMessage{
-				Timestamp: frozenTime,
-				Name:      "John Doe",
+			name: "success with two users entering and one leaving",
+			clientsIn: []struct{ id, name string }{
+				{"1", "Jane Doe"},
+				{"2", "John Doe"},
 			},
-			room: room,
+			clientsOut: []string{"2"},
+			expectedBroadcasts: func(room *application.ChatRoom, frozenTime time.Time) []Broadcast {
+				return []Broadcast{
+					{
+						room: room,
+						msg: &domain.UserLeftSystemMessage{
+							Timestamp: frozenTime,
+							Name:      "John Doe",
+						},
+					},
+				}
+			},
+			expectedClientsCount: 1,
 		},
 	}
 
-	spyNotifier := SpyNotifier{broadcasts: make([]Broadcast, 0)}
-	chatService := application.NewChatService(room, &spyNotifier, func() time.Time { return frozenTime }, 3, 3)
-	room.LetClientIn(domain.NewClient("1", "Jane Doe"))
-	room.LetClientIn(domain.NewClient("2", "John Doe"))
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
 
-	chatService.Start(ctx)
+			clientRegistry := application.NewClientRegistry()
+			room := application.NewChatRoom("1", "general", clientRegistry)
+			frozenTime := time.Date(2025, 12, 7, 0, 0, 0, 0, time.UTC)
 
-	chatService.LeaveRoom("1")
-	chatService.LeaveRoom("2")
+			spyNotifier := SpyNotifier{broadcasts: make([]Broadcast, 0)}
+			chatService := application.NewChatService(room, &spyNotifier, func() time.Time { return frozenTime }, 3, 3)
 
-	time.Sleep(50 * time.Millisecond)
+			for _, client := range tt.clientsIn {
+				room.LetClientIn(domain.NewClient(client.id, client.name))
+			}
 
-	for idx, broadcast := range spyNotifier.broadcasts {
-		userLeft, ok := broadcast.msg.(*domain.UserLeftSystemMessage)
-		if !ok {
-			t.Errorf("expected a user left message, got %T", broadcast)
-		}
-		expected := expectedBroadcasts[idx]
-		assert.Equal(t, expected.left, userLeft)
-		assert.Equal(t, expected.room, broadcast.room)
+			chatService.Start(ctx)
+
+			for _, client := range tt.clientsOut {
+				chatService.LeaveRoom(client)
+			}
+
+			time.Sleep(50 * time.Millisecond)
+
+			expectedBroadcasts := tt.expectedBroadcasts(room, frozenTime)
+			assert.Equal(t, len(expectedBroadcasts), len(spyNotifier.broadcasts))
+
+			for idx, broadcast := range spyNotifier.broadcasts {
+				userLeft, ok := broadcast.msg.(*domain.UserLeftSystemMessage)
+				if !ok {
+					t.Errorf("expected a user left message, got %T", broadcast)
+				}
+				assert.Equal(t, expectedBroadcasts[idx].msg, userLeft)
+				assert.Equal(t, expectedBroadcasts[idx].room, broadcast.room)
+			}
+
+			assert.Len(t, room.GetClients(), tt.expectedClientsCount)
+		})
 	}
-
-	assert.Len(t, room.GetClients(), 0)
 }
