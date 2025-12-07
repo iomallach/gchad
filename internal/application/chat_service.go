2b61f20459a63ce2a2f2b812b350f797f993e0b4
Alexander Butenko <a.butenko.o@gmail.com>
2025-12-07 22:13:32 +0100

    Handle messages asynchronously in the chat service

--- internal/application/chat_service.go
+++ internal/application/chat_service.go
@@ -13,14 +13,16 @@ type Notifier interface {
 type ChatService struct {
 	room     *ChatRoom // TODO: later needs to be a repository of chat rooms
 	events   chan domain.ApplicationEvent
+	messages chan *domain.UserMessage
 	notifier Notifier
 	clock    ClockGen
 }
 
-func NewChatService(room *ChatRoom, notifier Notifier, clock ClockGen, eventsChanSize int) *ChatService {
+func NewChatService(room *ChatRoom, notifier Notifier, clock ClockGen, eventsChanSize int, messagesChanSize int) *ChatService {
 	return &ChatService{
 		room:     room,
 		events:   make(chan domain.ApplicationEvent, eventsChanSize),
+		messages: make(chan *domain.UserMessage, messagesChanSize),
 		notifier: notifier,
 		clock:    clock,
 	}
@@ -28,6 +30,7 @@ func NewChatService(room *ChatRoom, notifier Notifier, clock ClockGen, eventsCha
 
 func (cs *ChatService) Start(ctx context.Context) {
 	go cs.handleEvents(ctx)
+	go cs.handleMessages(ctx)
 }
 
 func (cs *ChatService) EnterRoom(clientId string, clientName string) {
@@ -53,7 +56,25 @@ func (cs *ChatService) LeaveRoom(clientId string) {
 func (cs *ChatService) SendMessage(clientId string, msg string) error {
 	userMessage := domain.NewUserMessage(msg, cs.clock())
 
+	select {
+	case cs.messages <- userMessage:
+	default:
+		// TODO: log message channel full
+	}
 	return cs.notifier.BroadcastToRoom(cs.room, userMessage)
+}
+
+func (cs *ChatService) handleMessages(ctx context.Context) {
+	for {
+		select {
+		case msg := <-cs.messages:
+			if err := cs.notifier.BroadcastToRoom(cs.room, msg); err != nil {
+				// TODO: log error
+			}
+		case <-ctx.Done():
+			return
+		}
+	}
 }
 
 func (cs *ChatService) handleEvents(ctx context.Context) {
