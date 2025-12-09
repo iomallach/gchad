f97376268f7e0e42e3f4a92d52e2776f7c00b476
Alexander Butenko <a.butenko.o@gmail.com>
2025-12-09 20:42:29 +0100

    Adjust logger adapter implementation

--- internal/infrastructure/logger.go
+++ internal/infrastructure/logger.go
@@ -12,14 +12,32 @@ func NewZeroLogLogger(logger zerolog.Logger) *ZeroLogLogger {
 	}
 }
 
-func (l *ZeroLogLogger) Debug() *zerolog.Event {
-	return l.logger.Debug()
+func (l *ZeroLogLogger) Debug(msg string, fields map[string]any) {
+	event := l.logger.Debug()
+
+	for k, v := range fields {
+		event.Interface(k, v)
+	}
+
+	event.Msg(msg)
 }
 
-func (l *ZeroLogLogger) Info() *zerolog.Event {
-	return l.logger.Info()
+func (l *ZeroLogLogger) Info(msg string, fields map[string]any) {
+	event := l.logger.Info()
+
+	for k, v := range fields {
+		event.Interface(k, v)
+	}
+
+	event.Msg(msg)
 }
 
-func (l *ZeroLogLogger) Error() *zerolog.Event {
-	return l.logger.Error()
+func (l *ZeroLogLogger) Error(msg string, fields map[string]any) {
+	event := l.logger.Error()
+
+	for k, v := range fields {
+		event.Interface(k, v)
+	}
+
+	event.Msg(msg)
 }
