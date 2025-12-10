77df73c0c5771b1308253a7fa8fde9dc172095d6
Alexander Butenko <a.butenko.o@gmail.com>
2025-12-10 19:10:10 +0100

    Handle client lifeclycle in notifier's goroutine

--- internal/infrastructure/notifier.go
+++ internal/infrastructure/notifier.go
@@ -11,20 +11,29 @@ type Notifier interface {
 	application.Notifier
 	RegisterClient(*ClientAdapter)
 	UnregisterClient(string)
+	Start()
 }
 
 type ClientNotifier struct {
-	mu      sync.RWMutex
-	clients map[string]*ClientAdapter
-	logger  application.Logger
+	mu         sync.RWMutex
+	clients    map[string]*ClientAdapter
+	register   chan *ClientAdapter
+	unregister chan string
+	logger     application.Logger
 }
 
 func NewClientNotifier(logger application.Logger) *ClientNotifier {
 	return &ClientNotifier{
-		mu:      sync.RWMutex{},
-		clients: make(map[string]*ClientAdapter),
-		logger:  logger,
+		mu:         sync.RWMutex{},
+		clients:    make(map[string]*ClientAdapter),
+		register:   make(chan *ClientAdapter, 256),
+		unregister: make(chan string, 256),
+		logger:     logger,
 	}
+}
+
+func (n *ClientNotifier) Start() {
+	go n.handleClientLifecycle()
 }
 
 func (n *ClientNotifier) BroadcastToRoom(room *application.ChatRoom, msg domain.Messager) error {
@@ -53,18 +62,37 @@ func (n *ClientNotifier) BroadcastToRoom(room *application.ChatRoom, msg domain.
 	return nil
 }
 
-func (n *ClientNotifier) RegisterClient(adapter *ClientAdapter) {
-	n.mu.Lock()
-	defer n.mu.Unlock()
-
-	// TODO: handle already exists situation
-	n.clients[adapter.Id()] = adapter
+func (n *ClientNotifier) RegisterClient(client *ClientAdapter) {
+	select {
+	case n.register <- client:
+		n.logger.Debug("scheduled client to be registered", map[string]any{"client_id": client.Id()})
+	default:
+		n.logger.Error("channel is full or closed", map[string]any{"client_id": client.Id()})
+	}
 }
 
 func (n *ClientNotifier) UnregisterClient(id string) {
-	n.mu.Lock()
-	defer n.mu.Unlock()
+	select {
+	case n.unregister <- id:
+		n.logger.Debug("scheduled client to be unregistered", map[string]any{"client_id": id})
+	default:
+		n.logger.Error("channel is full or closed", map[string]any{"client_id": id})
+	}
+}
 
-	// TODO: handle doesn't exist situation
-	delete(n.clients, id)
+func (n *ClientNotifier) handleClientLifecycle() {
+	for {
+		select {
+		case client := <-n.register:
+			// TODO: handle already exists situation
+			n.mu.Lock()
+			n.clients[client.Id()] = client
+			n.mu.Unlock()
+		case clientId := <-n.unregister:
+			// TODO: handle doesn't exist situation
+			n.mu.Lock()
+			delete(n.clients, clientId)
+			n.mu.Unlock()
+		}
+	}
 }
