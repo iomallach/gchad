2d6e70407018e32f95ed3324c603bc1aceb2f1ab
Alexander Butenko <a.butenko.o@gmail.com>
2025-12-13 14:47:57 +0100

    Add the first version of the handler

---
+++ internal/infrastructure/handler.go
@@ -0,0 +1,90 @@
+package infrastructure
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/iomallach/gchad/internal/application"
+	"github.com/iomallach/gchad/internal/domain"
+)
+
+type Handler struct {
+	upgrader     websocket.Upgrader
+	chatService  *application.ChatService
+	notifier     *ClientNotifier
+	clientConfig ClientConfiguration
+	idGen        application.IdGen
+	logger       application.Logger
+	appCtx       context.Context
+}
+
+func NewHandler(
+	upgrader websocket.Upgrader,
+	chatService *application.ChatService,
+	notifier *ClientNotifier,
+	clientConfig ClientConfiguration,
+	idGen application.IdGen,
+	logger application.Logger,
+	appCtx context.Context,
+) *Handler {
+	return &Handler{
+		upgrader:     upgrader,
+		chatService:  chatService,
+		notifier:     notifier,
+		clientConfig: clientConfig,
+		idGen:        idGen,
+		logger:       logger,
+		appCtx:       appCtx,
+	}
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	conn, err := h.upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		h.logger.Error(fmt.Sprintf("upgrade failed: %s", err.Error()), map[string]any{})
+		return
+	}
+
+	clientName := r.URL.Query().Get("name")
+	if clientName == "" {
+		h.logger.Error("client name not provided, skipping", map[string]any{})
+		return
+	}
+
+	clientId := h.idGen()
+	wsConn := NewWebsocketsConnection(conn, h.logger)
+	recv := make(chan domain.Messager, h.clientConfig.RecvChannelSize)
+	send := make(chan domain.Messager, h.clientConfig.SendChannelSize)
+	client := NewClient(clientId, clientName, wsConn, recv, send, h.clientConfig, h.logger)
+
+	h.notifier.RegisterClient(client)
+	h.chatService.EnterRoom(clientId, clientName)
+
+	ctx, cancel := context.WithCancel(h.appCtx)
+
+	go client.WriteMessages(ctx)
+	go h.forwardMessages(ctx, clientId, recv)
+
+	client.ReadMessages(ctx)
+	cancel()
+	h.chatService.LeaveRoom(clientId)
+	h.notifier.UnregisterClient(clientId)
+}
+
+func (h *Handler) forwardMessages(ctx context.Context, clientId string, recv chan domain.Messager) {
+	for {
+		select {
+		case msg, ok := <-recv:
+			if !ok {
+				return
+			}
+			if userMsg, ok := msg.(*domain.UserMessage); ok {
+				h.chatService.SendMessage(clientId, userMsg.Message)
+			}
+		case <-ctx.Done():
+			return
+		}
+	}
+}
