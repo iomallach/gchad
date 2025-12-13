9bb0f3e5fdd85ca5115905745790def4e579437f
Alexander Butenko <a.butenko.o@gmail.com>
2025-12-13 15:07:33 +0100

    Rework main func, wire all components

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
-	"github.com/iomallach/gchad"
+	"github.com/iomallach/gchad/internal/application"
+	"github.com/iomallach/gchad/internal/infrastructure"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
@@ -18,59 +22,56 @@ const (
 	pingPeriod = (pongWait * 9) / 10
 )
 
-var upgrader websocket.Upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin:     func(r *http.Request) bool { return true },
-}
-
-func serveWs(hub *gchad.Hub, w http.ResponseWriter, r *http.Request) error {
-	log.Debug().Str("remote_addr", r.RemoteAddr).Msg("new websocket connection")
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Error().Err(err).Msg("failed to upgrade connection")
-		return err
-	}
-
-	body := make([]byte, 256)
-	read_bytes, err := r.Body.Read(body)
-	if err != nil {
-		log.Error().Err(err).Msg("failed to read request body")
-		return err
-	}
-
-	message, err := gchad.MapConnectMeMessage(body[:read_bytes])
-	if err != nil {
-		return err
-	}
-	hubNotifier := gchad.NewChannelHubNotifier(hub.Unregister, hub.Broadcast)
-	client := gchad.CreateClient(message, hubNotifier, conn, 256, func() string { return uuid.NewString() }, pingPeriod, writeWait)
-
-	gchad.LauchClient(hub, &client)
-
-	return nil
-}
-
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	logger := infrastructure.NewZeroLogLogger(log.Logger)
 
-	hub := gchad.NewHub(make(chan *gchad.Message), make(chan *gchad.Client), make(chan *gchad.Client), func() time.Time {
-		return time.Now()
-	})
-	go hub.Run()
-	log.Info().Msg("hub started")
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-sigChan
+		logger.Info("Received signal, shutting down", map[string]any{})
+		cancel()
+	}()
 
-	http.HandleFunc("/chat", func(w http.ResponseWriter, r *http.Request) {
-		err := serveWs(&hub, w, r)
-		if err != nil {
-			log.Error().Err(err).Msg("couldn't serve websocket request, skipping")
-		}
-	})
+	room := application.NewChatRoom("1", "General", application.NewClientRegistry())
+	// TODO: maybe the notifier shouldn't be exposed here at all, and shall handle
+	// registration calls via chat service telling it to do so?
+	notifier := infrastructure.NewClientNotifier(logger, make(map[string]*infrastructure.Client))
+	chatService := application.NewChatService(room, notifier, func() time.Time { return time.Now() }, 256, 256, logger)
 
-	log.Info().Str("addr", ":8080").Msg("server starting")
+	var upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin:     func(r *http.Request) bool { return true },
+	}
+	clientConfig := infrastructure.ClientConfiguration{
+		WriteWait:       10 * time.Second,
+		PongWait:        60 * time.Second,
+		PingPeriod:      (60 * 9 * time.Second) / 10,
+		RecieveChanWait: 10 * time.Second,
+		SendChannelSize: 256,
+		RecvChannelSize: 256,
+	}
+	handler := infrastructure.NewHandler(
+		upgrader,
+		chatService,
+		notifier,
+		clientConfig,
+		func() string { return uuid.NewString() },
+		logger,
+		ctx,
+	)
+
+	http.HandleFunc("/chat", handler.ServeHTTP)
+
+	logger.Info("server starting, serving the chat at /chat", map[string]any{"port": "8080"})
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		log.Fatal().Err(err).Msg("server failed")
+		logger.Error("server failed", map[string]any{"error": err.Error()})
 	}
 }
