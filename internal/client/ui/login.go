43ee7f9bae88bfe87ecd95979d04ad2138718c92
Alexander Butenko <a.butenko.o@gmail.com>
2025-12-14 10:37:20 +0100

    Implement simple login model

--- internal/client/ui/login.go
+++ internal/client/ui/login.go
@@ -1,6 +1,8 @@
 package ui
 
 import (
+	"fmt"
+
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 )
@@ -13,6 +15,7 @@ type Login struct {
 func InitialLoginModel(textAboveInput string) Login {
 	input := textinput.New()
 	input.CharLimit = 20
+	input.Width = 40
 	input.Placeholder = "Username"
 	input.Validate = func(string) error { return nil }
 
@@ -27,9 +30,26 @@ func (l Login) Init() tea.Cmd {
 }
 
 func (l Login) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	return l, nil
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		l.input.Focus()
+		switch msg.String() {
+		case "ctrl+c":
+			return l, tea.Quit
+		case "enter":
+			value := l.input.Value()
+			l.textAboveInput = fmt.Sprintf("going to connect as %s", value)
+			l.input.Reset()
+			return l, nil
+		}
+	}
+
+	var cmd tea.Cmd
+	l.input, cmd = l.input.Update(msg)
+
+	return l, cmd
 }
 
 func (l Login) View() string {
-	return ""
+	return fmt.Sprintf("%s\n\n%s", l.textAboveInput, l.input.View())
 }
