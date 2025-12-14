a3bf97e792cd29bc92c9d81959bb3266a6661b7f
Alexander Butenko <a.butenko.o@gmail.com>
2025-12-14 10:37:10 +0100

    Move entrypoints

---
+++ cmd/client/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/iomallach/gchad/internal/client/ui"
+)
+
+func main() {
+	model := ui.InitialLoginModel("Who are you?")
+	program := tea.NewProgram(model)
+	if _, err := program.Run(); err != nil {
+		fmt.Printf("there has been an error: %s", err.Error())
+	}
+}
