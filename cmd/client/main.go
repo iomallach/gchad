63016383e8e47d5c60d4d06c8d02ceef7169c2bc
Alexander Butenko <a.butenko.o@gmail.com>
2025-12-17 18:08:26 +0100

    Partially wire in client main

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,11 +4,24 @@ import (
 	"fmt"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/gorilla/websocket"
+	"github.com/iomallach/gchad/internal/client/infrastructure"
 	"github.com/iomallach/gchad/internal/client/ui"
 )
 
 func main() {
-	model := ui.InitialAppModel()
+	login := ui.InitialLoginModel("Who are you?", ui.DefaultLoginScreenKeymap)
+	chat := ui.InitialChatModel(ui.DefaultChatScreenKeymap)
+	// TODO: pass a logger instead of nils
+	dialer := infrastructure.NewWebsocketDialer(websocket.DefaultDialer, nil)
+	chatClient := infrastructure.NewChatClient(
+		dialer,
+		make(chan any, 256),
+		make(chan any, 256),
+		make(chan error, 256),
+		nil,
+	)
+	model := ui.InitialAppModel(login, chat, chatClient)
 	program := tea.NewProgram(model)
 	if _, err := program.Run(); err != nil {
 		fmt.Printf("there has been an error: %s", err.Error())
